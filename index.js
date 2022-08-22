f2644683008cdf8939fb37970d702beeeb1d13f8
Kevin Olivier <kevinolivier1111@gmail.com>
2022-08-22 14:38:33 -0600

    commit tree 79f3b95962f642040e6be191e1af9e3a349e56c9

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+destructivelyAppendCat(Ralph);
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+destructivelyPrependCat(Bob);
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+destructivelyRemoveLastCat();
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+destructivelyRemoveFirstCat();
+
+function appendCat(name) {
+    const copyofCats = [...cats, name];
+    return copyofCats;
+}
+appendCat(Broom);
+
+function prependCat(name) {
+    const copyofCats = [name, ...cats];
+    return copyofCats;
+}
+prependCat(Arnold);
+
+function removeLastCat() {
+    const copyofCats = [...cats.slice(0,2)];
+    return copyofCats;
+}
+removeLastCat();
+
+function removeFirstCat() {
+    const copyofCats = [...cats.slice(-2)];
+    return copyofCats;
+}
+removeFirstCat();
